c6d62693a45e66864cd48e516e74b6996fc07f98
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-11-30 23:50:29 -0500

    Fixed playlist model so that songs are referenced not embedded like the previous homeworks.

--- server/models/playlist-model.js
+++ server/models/playlist-model.js
@@ -3,21 +3,27 @@ const Schema = mongoose.Schema
 /*
     This is where we specify the format of the data we're going to put into
     the database.
-    
-    @author McKilla Gorilla
+
+    - Each playlist has unique name per owner
+    - Store ordered array of Song references
+    - Track distinct listeners who have played the playlist 
 */
 const playlistSchema = new Schema(
     {
-        name: { type: String, required: true },
+        name: { type: String, required: true , trim: true},
+        owner: { type: Schema.Types.ObjectId, ref: 'User', required: true},
         ownerEmail: { type: String, required: true },
-        songs: { type: [{
-            title: String,
-            artist: String,
-            year: Number,
-            youTubeId: String
-        }], required: true }
+        songs: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Song'
+        }],
+        listeners:[{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
     },
     { timestamps: true }
 )
 
+playlistSchema.index({name: 1, owner: 1}, {unique: true})
 module.exports = mongoose.model('Playlist', playlistSchema)
