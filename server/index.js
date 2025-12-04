fa9ab9c6c4040406e7a86f8c8e386337b8f4d455
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-04 02:11:32 -0500

    Fixed the Routers middleware part : storeRouter -> /api and song Router addedd

--- server/index.js
+++ server/index.js
@@ -22,7 +22,9 @@ app.use(cookieParser())
 const authRouter = require('./routes/auth-router')
 app.use('/auth', authRouter)
 const storeRouter = require('./routes/store-router')
-app.use('/store', storeRouter)
+app.use('/api', storeRouter)
+const songRouter = require('./routes/song-router')
+app.use('/api', songRouter)
 
 // INITIALIZE OUR DATABASE OBJECT
 const dbManager = require('./db/loader')
