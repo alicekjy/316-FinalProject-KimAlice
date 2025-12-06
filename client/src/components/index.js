8b5682394ec22767b1082d6d96a83e2a3fe9a8b5
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 18:55:40 -0500

    Creating components index.js to export all components

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import SplashScreen from './SplashScreen';
+import LoginScreen from './LoginScreen';
+import RegisterScreen from './RegisterScreen';
+
+export {
+    SplashScreen,
+    LoginScreen,
+    RegisterScreen
+}
