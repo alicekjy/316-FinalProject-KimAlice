52b41d71f03c28a43875548d87abc62998dad93d
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 18:31:56 -0500

    Fixed App.js accordingly  to the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './auth';
 
-function App(){
-    return(
-        <div style = {{padding: '50px', textAlign: 'center', color: 'white'}}>
-            <h1>🎵 Playlister</h1>
-            <p>React app is working</p>
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <div style={{ padding: '50px', textAlign: 'center', color: 'white' }}>
+          <h1>🎵 Playlister</h1>
+          <p>React app is working!</p>
+          <p>Auth context loaded successfully!</p>
         </div>
-    );
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
-export default App; 
+
+export default App;
