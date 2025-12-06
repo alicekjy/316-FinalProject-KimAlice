12d33002f0daa822bb5de3f7ff481a549ede6f7f
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 01:30:49 -0500

    Added app.js to the client

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(){
+    return(
+        <div style = {{padding: '50px', textAlign: 'center', color: 'white'}}>
+            <h1>🎵 Playlister</h1>
+            <p>React app is working</p>
+        </div>
+    );
+}
+export default App; 
