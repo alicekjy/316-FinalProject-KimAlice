daabecc48fc3959a79adcc67228f247fa064646c
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 21:28:10 -0500

    playlist functions

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,6 +2,7 @@ import {createContext, useContext, useState} from 'react'
 import {useHistory} from 'react-router-dom'
 import api from './requests'
 import AuthContext from '../auth'
+import { typography } from '@mui/system';
 
 const GlobalStoreContext = createContext({});
 
@@ -11,4 +12,138 @@ export const GlobalStoreActionType = {
     CLOSE_CURRENT_PLAYLIST: "CLOSE_CURRENT_PLAYLIST",
     LOAD_SONGS: "LOAD_SONGS",
     SET_CURRENT_PLAYLIST: "SET_CURRENT_SONG"
+}
+
+function GlobalStoreContextProvider(props) {
+    const {auth} = useContext(AuthContext);
+    const history = useHistory();
+
+    const [store,setStore] = useState({
+        playlists: [],
+        currentPlaylist: null,
+        songs: [],
+        currentSong: null
+    });
+
+    const storeReducer = (action) => {
+        const {type, payload} = action;
+        switch(type){
+            case GlobalStoreActionType.LOAD_PLAYLISTS:
+                return setStore({
+                    ...store,
+                    playlists: payload
+                });
+            case GlobalStoreActionType.SET_CURRENT_PLAYLIST:
+                return setStore({
+                    ...store,
+                    currentPlaylist: payload
+                });
+            case GlobalStoreActionType.CLOSE_CURRENT_PLAYLIST:
+                return setStore({
+                    ...store,
+                    currentPlaylist: null
+                });
+            case GlobalStoreActionType.LOAD_SONGS:
+                return setStore({
+                    ...store,
+                    songs: payload
+                });
+            case GlobalStoreActionType.SET_CURRENT_SONG:
+                return setStore({
+                    ...store,
+                    currentSong: payload
+                });
+            default:
+                return store;
+        }
+    }
+    //playlist functions
+    store.loadPlaylists = async function () {
+        try{
+            const response = await api.getPlaylists();
+            if(response.status === 200){
+                storeReducer({
+                    type: GlobalStoreActionType.LOAD_PLAYLISTS,
+                    payload: response.data.playlists
+                });
+            }
+        }catch(error){
+            console.error('Failed to load playlists: ', error);
+        }
+    }
+
+    store.createPlaylist = async function () {
+        try{
+            const response = await api.createPlaylist();
+            if(response.status === 201){
+                store.loadPlaylists();
+            }
+        }catch(error){
+            console.error('Failed to create playlist: ', error);
+        }
+    }
+
+    store.deletePlaylist = async function (id) {
+        try{
+            const response = await api.deletePlaylistById(id);
+            if(response.status === 200){
+                store.loadPlaylists();
+            }
+        }catch (error){
+            console.error('Failed to delete playlist: ', error);
+        }
+    }
+
+    store.setCurrentPlaylist = async function (id) {
+        try{
+            const response = await api.getPlaylistById(id);
+            if(response.status === 200){
+                storeReducer({
+                    type: GlobalStoreActionType.SET_CURRENT_PLAYLIST,
+                    payload: response.data.playlist
+                });
+                history.push(`/playlist/${id}`);
+            }
+        }catch(error){
+            console.error('Failed to load playlist: ', error);
+        }
+    }
+
+    store.updatePlaylist = async function (id, name, songs){
+        try{
+            const response = await api.updatePlaylistById(id, name,songs);
+            if(response.status === 200){
+                store.loadPlaylists();
+                if(store.currentPlaylist && store.currentPlaylist._id === id){
+                    storeReducer({
+                        type: GlobalStoreActionType.SET_CURRENT_PLAYLIST,
+                        payload: response.data.playlist
+                    });
+                }
+            }
+        }catch (error){
+            console.error('Failed to update playlist: ', error);
+        }
+    }
+
+    store.copyPlaylist = async function (id){
+        try{
+            const response = await api.copyPlaylist(id);
+            if(response.status === 201){
+                store.loadPlaylists();
+            }
+        }catch (error){
+            console.error('Failed to copy playlist: ', error);
+        }
+    }
+
+    store.closeCurrentPlaylist = function(){
+        storeReducer({
+            type: GlobalStoreActionType.CLOSE_CURRENT_PLAYLIST,
+            payload: null
+        });
+        history.push('/home');
+    }
+
+    
 }
