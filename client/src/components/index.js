261debeebb29704cf46eeec15c8063b011be5e0c
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 22:41:06 -0500

    Updated components index to export new components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,13 @@
 import SplashScreen from './SplashScreen';
 import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
+import AppBanner from './AppBanner';
+import HomeScreen from './HomeScreen';
 
 export {
     SplashScreen,
     LoginScreen,
-    RegisterScreen
+    RegisterScreen,
+    AppBanner,
+    HomeScreen
 }
