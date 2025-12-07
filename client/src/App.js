d078c5e11664b2369bb5071fecdb4979bf6ab59a
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 22:44:40 -0500

    Fixed app.js accordingly to the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AuthContextProvider } from './auth';
+import { GlobalStoreContextProvider } from './store';
 import {
     SplashScreen,
     LoginScreen,
-    RegisterScreen
+    RegisterScreen,
+    AppBanner,
+    HomeScreen
 } from './components';
 
 function App() {
     return (
         <BrowserRouter>
             <AuthContextProvider>
-                <Switch>
+                <GlobalStoreContextProvider>
+                    <AppBanner />
+                    <Switch>
                     <Route path="/" exact component={SplashScreen} />
                     <Route path="/login" exact component={LoginScreen} />
                     <Route path="/register" exact component={RegisterScreen} />
+                    <Route path = "/home" exact component = {HomeScreen} />
                 </Switch>
+                </GlobalStoreContextProvider>
             </AuthContextProvider>
         </BrowserRouter>
     );
