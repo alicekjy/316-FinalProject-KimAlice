70015102e0ab92d889301869d4e3e4f4cc7181c4
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 23:47:02 -0500

    Updated with the new components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,11 +3,15 @@ import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
 import AppBanner from './AppBanner';
 import HomeScreen from './HomeScreen';
+import PlaylistScreen from './PlaylistScreen';
+import SongScreen from './SongScreen';
 
 export {
     SplashScreen,
     LoginScreen,
     RegisterScreen,
     AppBanner,
-    HomeScreen
+    HomeScreen,
+    PlaylistScreen,
+    SongScreen
 }
