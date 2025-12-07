a75b538d463bc1a7f7d70e8cbebdd368831a8af4
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2025-12-06 21:44:56 -0500

    Slight changes for the appbanner

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -1,5 +1,5 @@
-import {useContext, useState} from 'react';
-import {Link, useHistory} from 'react-router-dom'
+import { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
 import AuthContext from '../auth';
 import GlobalStoreContext from '../store';
 
@@ -10,4 +10,155 @@ import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import HomeIcon from '@mui/icons-material/Home';
+
+export default function AppBanner() {
+    const { auth } = useContext(AuthContext);
+    const { store } = useContext(GlobalStoreContext);
+    const history = useHistory();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleLogout = () => {
+        handleMenuClose();
+        auth.logoutUser();
+    }
+
+    const handleHome = () => {
+        if (store.currentPlaylist) {
+            store.closeCurrentPlaylist();
+        }
+        history.push('/home');
+    }
+
+    const menuId = 'primary-search-account-menu';
+    
+    const loggedOutMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>
+                <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
+                    Login
+                </Link>
+            </MenuItem>
+            <MenuItem onClick={handleMenuClose}>
+                <Link to='/register' style={{ textDecoration: 'none', color: 'inherit' }}>
+                    Create Account
+                </Link>
+            </MenuItem>
+        </Menu>
+    );
+
+    const loggedInMenu = 
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>        
+
+    let menu = loggedOutMenu;
+    if (auth.loggedIn) {
+        menu = loggedInMenu;
+    }
+    
+    function getAccountMenu(loggedIn) {
+        if (loggedIn) {
+            let userInitials = auth.getUserInitials();
+            return (
+                <Box sx={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: '50%',
+                    bgcolor: 'secondary.main',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    fontSize: '1rem',
+                    fontWeight: 'bold'
+                }}>
+                    {userInitials}
+                </Box>
+            );
+        } else {
+            return <AccountCircle />;
+        }
+    }
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="home"
+                        onClick={handleHome}
+                        sx={{ mr: 2 }}
+                    >
+                        <HomeIcon />
+                    </IconButton>
+                    
+                    <Typography                        
+                        variant="h4"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, cursor: 'pointer' }}
+                        onClick={handleHome}
+                    >
+                        🎵 Playlister
+                    </Typography>
+                    
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <IconButton
+                            size="large"
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            {getAccountMenu(auth.loggedIn)}
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {menu}
+        </Box>
+    );
+}
